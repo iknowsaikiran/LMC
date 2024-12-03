c96a2cb3363e9d3c025da50875028b9b1717ac7e
Your Name <youremail@example.com>
2024-12-03 12:09:33 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, json, jsonify
 from flask_mysqldb import MySQL
 from math import radians, cos, sin, asin, sqrt
-from flask_mysqldb import MySQL
 import logging
 import os
 from werkzeug.utils import secure_filename
@@ -13,9 +12,7 @@ app.secret_key = 'your_secret_key'
 # MySQL configurations
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-
 app.config['MYSQL_PASSWORD'] = 'Saty@136'
-
 app.config['MYSQL_DB'] = 'hospital'
 mysql = MySQL(app)
 
